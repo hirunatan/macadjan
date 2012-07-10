df95399d41dfb1589eb21f4468d6c8486b0fd4fb
Andrei Antoukh <niwi@niwi.be>
2012-07-10 17:03:35 +0200

    Add basic static files.

---
+++ macadjan/static/js/base.js
@@ -0,0 +1,65 @@
+/* Author:
+    RedDelicious
+    Company: Kaleidos Open Source
+*/
+
+/* gettext dummy wrapper if not exists */
+
+if (window.gettext === undefined) {
+    window.gettext = function(text) {
+        return text;
+    };
+}
+
+if (window.interpolate === undefined) {
+    window.interpolate = function(fmt, obj, named) {
+        if (named) {
+            return fmt.replace(/%\(\w+\)s/g, function(match){return String(obj[match.slice(2,-2)])});
+        } else {
+            return fmt.replace(/%s/g, function(match){return String(obj.shift())});
+        }
+    };
+}
+
+$(document).ajaxSend(function(event, xhr, settings) {
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    function sameOrigin(url) {
+        // url could be relative or scheme relative or absolute
+        var host = document.location.host; // host + port
+        var protocol = document.location.protocol;
+        var sr_origin = '//' + host;
+        var origin = protocol + sr_origin;
+        // Allow absolute or scheme relative URLs to same origin
+        return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+            (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+            // or any other URL that isn't scheme relative or absolute i.e relative.
+            !(/^(\/\/|http:|https:).*/.test(url));
+    }
+
+    function safeMethod(method) {
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+
+    if (!safeMethod(settings.type) && sameOrigin(settings.url)) {
+        xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+    }
+});
+
+
+/* Global macadjan  module namespace. */
+
+var Macadjan = {}
