def5e194e26c576666345024d30347f8eefddcff
Andrei Antoukh <niwi@niwi.be>
2012-07-10 17:16:37 +0200

    Improvements on admin.

--- macadjan/admin.py
+++ macadjan/admin.py
@@ -3,8 +3,18 @@
 from django.contrib import admin
 from . import models
 
+
+class SubCategoryInline(admin.StackedInline):
+    model = models.SubCategory
+    prepopulated_fields = {"slug": ("name",)}
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [
+        SubCategoryInline,
+    ]
+
 admin.site.register(models.EntityType)
-admin.site.register(models.Category)
+admin.site.register(models.Category, CategoryAdmin)
 admin.site.register(models.SubCategory)
 admin.site.register(models.TagCollection)
 admin.site.register(models.EntityTag)
