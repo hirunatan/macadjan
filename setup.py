cb39c8b7e75a5662e50993cfe9ca2e66bccf7b14
Andrés Moya <andres.moya@kaleidos.net>
2012-07-10 10:55:14 +0200

    Script to calculate version

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 #! /usr/bin/env python
 from setuptools import setup, find_packages
 
-# dynamic retrive version number from macadjan.VERSION
-version_tuple = __import__('macadjan').VERSION
-version = '.'.join([str(v) for v in version_tuple])
-
 CLASSIFIERS = [
     'Development Status :: 3 - Alpha',
     'Environment :: Web Environment',
@@ -20,7 +16,12 @@ CLASSIFIERS = [
     'Topic :: Software Development :: Libraries :: Python Modules',
 ]
 
+SETUP_REQUIREMENTS = [
+    'versiontools >= 1.8',
+]
+
 REQUIREMENTS = [
+    'distribute',
     'Django >= 1.3',
 ]
 
@@ -32,12 +33,13 @@ setup(name='macadjan',
       author_email='hirunatan@hammo.org',
       description='Mapping and Catalog with Django (entities searchable by categories, keywords or geographic data)',
       license='AGPLv3+',
-      version=version,
+      version=':versiontools:macadjan:',
       url='https://github.com/hirunatan/macadjan',
       packages=find_packages(exclude=['doc*', 'examples*', 'tests*',
                                       'website*']),
       include_package_data=True,
       classifiers=CLASSIFIERS,
+      setup_requires=SETUP_REQUIREMENTS,
       install_requires=REQUIREMENTS,
       extras_require=EXTRAS,
       platforms=['any'],
