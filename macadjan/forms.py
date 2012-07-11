28abfbe3984c7f1e9e4a393a1ed5ad8d33846fea
tere <teresa.delatorre@kaleidos.net>
2012-07-11 15:09:41 +0200

    OpenLayersTileArgumentsForm and views

--- macadjan/forms.py
+++ macadjan/forms.py
@@ -9,3 +9,30 @@ class ParamsValidationForm(forms.Form):
     subcat = forms.IntegerField(required=False)
     zoom = forms.IntegerField(required=False)
 
+class OpenLayersTileArgumentsForm(forms.Form):
+    '''
+    Process filters for the markers in a map tile.
+    '''
+    bbox = forms.CharField(required = True)          # contain 'left,right,top,bottom'
+    features = forms.CharField(required = True)      # features
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        bbox = cleaned_data.get("bbox")
+
+        if bbox:
+            bbox_split = bbox.split(',')
+            if not len(bbox_split) == 4:
+                # TODO: Error
+                pass
+
+            for i, name in enumerate(['left', 'bottom', 'right', 'top']):
+                coord = bbox_split[i]
+                try:
+                    float(bbox_split[i])
+                    cleaned_date[name] = coord
+                except TypeError:
+                    # TODO: Error
+                    pass
+
+        return cleaned_data
